603ff1a1f3bcbe6912ff34c7423a927cfcedf965
harun-u-r-rashid <hr100756@gmail.com>
2024-06-15 16:54:26 +0600

    first-commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// function toggleMenu(){
+//         const menu = document.querySelector(".menuLinks");
+//         const icon = document.querySelector(".resIcon");
+//         menu.classList.toggle("open");
+//         icon.classList.toggle("open");
+// }
+
+
+function toggleMenu() {
+  const menu = document.querySelector(".menuLinks");
+  const icon = document.querySelector(".resIcon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+
